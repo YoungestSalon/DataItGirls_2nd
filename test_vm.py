ade6c0c8985bbc05a514a17d4a6f00c870393d28
youngestsalon <youngestsalon@gmail.com>
2018-08-14 17:09:56 +0900

    Add VendingMachine Class

--- test_vm.py
+++ test_vm.py
@@ -1,26 +1,27 @@
-from vm import run, init
+# from vm import run, init, VendingMachine
+from vm import VendingMachine
 
 
 # 최초의 잔액 테스트
 def test_initial_change_sholud_be_zero():
-    init()
-    assert "잔액은 0원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "잔액은 0원입니다" == m.run("잔액")
 
 # 동전을 넣으면 잔액이 올라가는가?
 def test_insert_coin_and_check():
-    init()
-    assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 100원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 # 동전을 두 번 넣으면 잔액이 그만큼 올라가는가?
 def test_accumulation_of_change():
-    init()
-    run("동전 100")
-    run("동전 100")
-    assert "잔액은 200원입니다" == run("잔액")
+    m = VendingMachine()
+    m.run("동전 100")
+    m.run("동전 100")
+    assert "잔액은 200원입니다" == m.run("잔액")
 
 # 잔액/동전 외의 명령어를 넣으면 "알 수 없는 명령입니다."가 나오는가?
 def test_unknown():
-    init()
-    assert "알 수 없는 명령입니다." == run("달러")
-    assert "알 수 없는 명령입니다." == run("돈내놔")
+    m = VendingMachine()
+    assert "알 수 없는 명령입니다." == m.run("달러")
+    assert "알 수 없는 명령입니다." == m.run("돈내놔")
--- vm.py
+++ vm.py
@@ -1,25 +1,54 @@
-change = 0
+# VendingMachine이라는 새로운 개념이 생김
+class VendingMachine:
+
+    # VendingMachine의 초기값을 설정해줌.
+    # test_vm.py에서 'm = VendingMachine()'을 실행하는 순간 실체(인스턴스)가 생김
+    # self : 새로 생긴 실체(인스턴스) 그 자체를 가리키며, 자아(=초기값)를 부여해줌
+    # _change : change 앞의 under-bar(_) = '클래스 정의 부분 밖에서 임의로 change 호출/변경 금지'를 의미
+    # __init__ : under-bar가 2개(__) = 'Python 내장 문법이기 때문에 절대 임의로 변경하지 말 것'을 의미
+    def __init__(self):
+        self._change = 0
+
+    # 메소드(= 특정 클래스에서만 사용 가능한 함수)를 정의해줌.
+    # 모든 메소드의 첫번째 호출 인자는 self(인스턴스 그 자체)여야 함.
+    def run(self, raw):
+        tokens = raw.split(" ")
+        cmd, params = tokens[0], tokens[1:]
+
+        # if cmd not in ["잔액", "동전"]:
+        #     return "알 수 없는 명령입니다"
+        if cmd == "잔액":
+            return "잔액은 " + str(self._change) + "원입니다"
+        elif cmd == "동전":
+            coin = params[0]
+            self._change += int(coin)
+            return coin + "원을 넣었습니다"
+        else:
+            return "알 수 없는 명령입니다."
+            pass
 
 
-# Test Isolation을 위한 함수
-def init():
-    global change
-    change = 0
-
-# Main Code
-def run(raw):
-    global change
-
-    tokens = raw.split(" ")
-    cmd, params = tokens[0], tokens[1:]
-
-    # if cmd not in ["잔액", "동전"]:
-    #     return "알 수 없는 명령입니다"
-    if cmd == "잔액":
-        return "잔액은 " + str(change) + "원입니다"
-    elif cmd == "동전":
-        coin = params[0]
-        change += int(coin)
-        return coin + "원을 넣었습니다"
-    else:
-        return "알 수 없는 명령입니다."
+# change = 0
+#
+# # Test Isolation을 위한 함수
+# def init():
+#     global change
+#     change = 0
+#
+# # Main Code
+# def run(raw):
+#     global change
+#
+#     tokens = raw.split(" ")
+#     cmd, params = tokens[0], tokens[1:]
+#
+#     # if cmd not in ["잔액", "동전"]:
+#     #     return "알 수 없는 명령입니다"
+#     if cmd == "잔액":
+#         return "잔액은 " + str(change) + "원입니다"
+#     elif cmd == "동전":
+#         coin = params[0]
+#         change += int(coin)
+#         return coin + "원을 넣었습니다"
+#     else:
+#         return "알 수 없는 명령입니다."
